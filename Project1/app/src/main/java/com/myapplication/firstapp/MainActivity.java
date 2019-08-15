fc41b2a885dcbef993c75c9c3a1310d3a2059f51
tswazir <sachwazir25@gmail.com>
2019-08-15 15:38:55 -0500

    4th commit

--- Project1/app/src/main/java/com/myapplication/firstapp/MainActivity.java
+++ Project1/app/src/main/java/com/myapplication/firstapp/MainActivity.java
@@ -3,7 +3,7 @@ package com.myapplication.firstapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
